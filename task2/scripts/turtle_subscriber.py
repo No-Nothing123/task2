79cba69be95c8e746c92387e18236b251eee6d38
No-Nothing123 <meetballlyx@gmail.com>
2020-12-31 14:12:39 +0900

    Add a file

--- task2/scripts/turtle_subscriber.py
+++ task2/scripts/turtle_subscriber.py
@@ -6,10 +6,10 @@ from turtlesim.msg import Pose
 def poseCallback(msg):
     rospy.loginfo("x : %0.5f,y : %0.5f",msg.x,msg.y)
 
-def turtle_subscriber():
-    rospy.init_node('pose_subscriber',anonymous=True)
-    rospy.Subscriber("/turtle1/pose",Pose,poseCallback)
-    rospy.spin()
-
 if __name__ == '__main__':
-    turtle_subscriber()
+    rospy.init_node('turtle_subscriber',anonymous = True)
+    sub = rospy.Subscriber("/turtle1/pose",Pose,poseCallback)
+    rospy.spin()
+    
+
+	
