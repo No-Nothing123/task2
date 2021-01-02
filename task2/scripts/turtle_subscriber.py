5ff60e65006e6a4cb1820b901654eadda601ecdf
No-Nothing123 <58504093+No-Nothing123@users.noreply.github.com>
2021-01-02 13:18:37 +0900

    Update turtle_subscriber.py

--- task2/scripts/turtle_subscriber.py
+++ task2/scripts/turtle_subscriber.py
@@ -7,7 +7,7 @@ def poseCallback(msg):
     rospy.loginfo("x : %0.5f,y : %0.5f",msg.x,msg.y)
 
 if __name__ == '__main__':
-    rospy.init_node('turtle_subscriber',anonymous = True)
+    rospy.init_node('turtle_subscriber')
     sub = rospy.Subscriber("/turtle1/pose",Pose,poseCallback)
     rospy.spin()
     
