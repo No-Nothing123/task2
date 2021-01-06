d66f17521e64a045329352e440346d7ebb081aee
No-Nothing123 <58504093+No-Nothing123@users.noreply.github.com>
2021-01-06 23:23:54 +0900

    Add files via upload

---
+++ scripts/turtle_subscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import rospy
+from turtlesim.msg import Pose
+
+def poseCallback(msg):
+    rospy.loginfo("x : %0.5f,y : %0.5f",msg.x,msg.y)
+
+if __name__ == '__main__':
+    rospy.init_node('turtle_subscriber')
+    sub = rospy.Subscriber("/turtle1/pose",Pose,poseCallback)
+    rospy.spin()
+    
+
+	
